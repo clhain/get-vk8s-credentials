9bdec69331915b1c150d5e809d94bd887e2c0a98
Chris Hain <c.hain@f5.com>
2021-12-07 11:59:07 -0800

    adjust for urlencoded outut

--- get-creds.sh
+++ get-creds.sh
@@ -6,7 +6,7 @@ do
     if [[ $line =~ "name: " ]]; then
         NAME=${line:6}
     fi
-done < <($VOLTERRA_CLI_PATH/vesctl request rpc api_credential.CustomAPI.Create -i $TEMPLATE --uri /public/namespaces/system/api_credentials --http-method POST --timeout 10)
+done < <($VOLTERRA_CLI_PATH/vesctl request rpc api_credential.CustomAPI.Create -i $TEMPLATE --uri /public/namespaces/system/api_credentials --http-method POST --timeout 10 | sed 's/%0A/\n/g')
 
 mkdir -p ~/.kube/
 
